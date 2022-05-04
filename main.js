2e7fac1f406b2073b3beee115607b2518438cb52
Hugo de Araujo <hugo.saraujo@tutanota.com>
2022-05-04 13:14:10 -0300

    Criação de eventos para navegar no aluramidi com teclado

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
 
     const teclas = listaDeTeclas[i];
     const instrumento = teclas.classList[1];
-    const idAudio = `#som_${instrumento}`
+    const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[i].onclick = () => {
+    teclas.onclick = () => {
         tocaSom(idAudio);
-    };
+    }
+
+    teclas.onkeydown = () => {
+        teclas.classList.add('ativa');
+    }
 }
